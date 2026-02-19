dcb8568380e2454fefbb91b4e828cc0faa21fef7
vedant-asati03 <vedant.asati03@gmail.com>
2026-02-19 22:45:47 +0530

    Added new colors to the palette

--- code/utilities/colors.py
+++ code/utilities/colors.py
@@ -2,8 +2,20 @@ from enum import Enum
 
 
 class Colors(Enum):
+    # Darks & Neutrals
+    DEEP_OCEAN = "#1a2b3c"
+    JET_BLACK = "#293241"
+    GHOST_WHITE = "#f0f7f9"
+
+    # Blues
     DUSK_BLUE = "#3d5a80"
+    STEEL_BLUE = "#5b8db8"
     POWDER_BLUE = "#98c1d9"
     LIGHT_CYAN = "#e0fbfc"
+
+    # Teals
+    SEAFOAM = "#2ec4b6"
+
+    # Warms
+    WARM_SAND = "#e9c46a"
     BURNT_PEACH = "#ee6c4d"
-    JET_BLACK = "#293241"
