f02013e9a661cb3c8a9396ee632beb1b9086e4f7
vedant-asati03 <vedant.asati03@gmail.com>
2026-02-19 19:59:59 +0530

    Add Colors enum to define a palette for the application

---
+++ code/utilities/colors.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Colors(Enum):
+    DUSK_BLUE = "#3d5a80"
+    POWDER_BLUE = "#98c1d9"
+    LIGHT_CYAN = "#e0fbfc"
+    BURNT_PEACH = "#ee6c4d"
+    JET_BLACK = "#293241"
